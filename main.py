3124d7284d6def25422b72f2f5ee6904087c7efe
Sebas <Sebastian_Rieger@outlook.de>
2022-04-16 05:06:46 +0200

    commit it and then push I guess

--- main.py
+++ main.py
@@ -19,4 +19,5 @@ if __name__ == '__main__':
     print_hi("worked ")
     print_hi("not ")
     print_hi("again ")
+    print_hi("very nice ")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
